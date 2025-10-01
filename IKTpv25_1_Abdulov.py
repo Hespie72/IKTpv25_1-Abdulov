7d41998508ba995e6ce98a46b552b54dc1bb14f6
opilane <opilane@HP-CZC2349HTD>
2025-10-01 11:42:18 +0300

    Harjadus 1, Ülessane 1, 2, 3, 4

--- IKTpv25_1_Abdulov.py
+++ IKTpv25_1_Abdulov.py
@@ -13,11 +13,12 @@
 # Küsi kasutajalt sisendit, mitu kommi ta soovib laualt ära võtta.
 # Eemalda soovitud kommide arv laual olevate kommide arvust ja kuva ekraanile,
 # kui palju komme laual nüüd on.
-from random import *
-laua_peal=randint(10, 50) #juhuslik arv 10-50
-print(f"Laual on {laua_peal} kommi")
-võtab=int(input("Mitu kommi sa soovid võtta? ")) #sesind võtab teisendab stringi täisarvuks
-laua_peal=laua_peal-võtab
+#from random import *
+#laua_peal=randint(10, 50) #juhuslik arv 10-50
+#print(f"Laual on {laua_peal} kommi")
+#võtab=int(input("Mitu kommi sa soovid võtta? ")) #sesind võtab teisendab stringi täisarvuks
+#laua_peal-=võtab #laua_peal=laua_peal-võtab, võtab kommid laualt maha
+#print(f"Laual on nüüd {laua_peal} kommi")
 
 
 
@@ -30,11 +31,23 @@ laua_peal=laua_peal-võtab
 # d) kas_on_koolis = True
 # Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende
 # Kirjuta kood tüüpide kontrollimiseks.
-vanus = 18          #int
-eesnimi = "Jaak"    #str
-pikkus = 1.65      #float
-kas_kaib_koolis = True  #bool
-print(f"vanus {vanus} on: {type(vanus)}")
-print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
-print(f"pikkus {pikkus} on: {type(pikkus)}")
-print(f"kas_kaib_koolis {kas_kaib_koolis} on: {type(kas_kaib_koolis)}")
+#vanus = 18          #int
+#eesnimi = "Jaak"    #str
+#pikkus = 1.65      #float
+#kas_kaib_koolis = True  #bool
+#print(f"vanus {vanus} on: {type(vanus)}")
+#print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
+#print(f"pikkus {pikkus} on: {type(pikkus)}")
+#print(f"kas_kaib_koolis {kas_kaib_koolis} on: {type(kas_kaib_koolis)}")
+
+#4.
+#import math 
+#
+#umbermoodu = float(input("Sisseta puu umbermoot (cm): "))
+#diameeter = umbermoodu / math.pi
+#print("Puu diameeter on:", round(diameeter, 2), "cm")
+
+#5.
+import = math
+
+print("Mitu Nm on:" rectangle(diameter), "m")
