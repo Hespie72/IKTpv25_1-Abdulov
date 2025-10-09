8a5db7b3b0bd2c686279728ef63252d66d60ec48
opilane <opilane@HP-CZC2349HT3>
2025-10-09 09:41:41 +0300

    Tund 2/Töö 1.2 Iseseisev töö "Vigade otsing -1"

--- IKTpv25_1_Abdulov.py
+++ IKTpv25_1_Abdulov.py
@@ -1,112 +1,5 @@
-#from os import PathLike
+#ülesanne 1
 
-
-#print ("Tere maailm!")
-#nimi=input("Sisesta oma nimi: ").capitalize()# sisend ja ootab enterit
-#print(f"Tere maailm! Tervitan sind {nimi}")
-#vanus=int(input("Sisesta oma vanus: "))#int teisendab stringi täisarvuks
-#print(f"Tere maalim! Tervitan sind {nimi.upper{}}. sa oled {vanus} aastat vana") #upper teeb suurtähed
-#print(f"Tere maalim! Tervitan sind {nimi.lower{}}. sa oled {vanus} aastat vana") #lower teeb väiketähed
-
-# Kirjuta enda koodis laual olevate kommide arv muutujasse(kommide arv on juhuslik). 
-# Seejärel kuva muutujas olev kommide arv ekraanile kasutades print() käsku.
-# Küsi kasutajalt sisendit, mitu kommi ta soovib laualt ära võtta.
-# Eemalda soovitud kommide arv laual olevate kommide arvust ja kuva ekraanile,
-# kui palju komme laual nüüd on.
-#from random import *
-#laua_peal=randint(10, 50) #juhuslik arv 10-50
-#print(f"Laual on {laua_peal} kommi")
-#võtab=int(input("Mitu kommi sa soovid võtta? ")) #sesind võtab teisendab stringi täisarvuks
-#laua_peal-=võtab #laua_peal=laua_peal-võtab, võtab kommid laualt maha
-#print(f"Laual on nüüd {laua_peal} kommi")
-
-
-
-
-# 2.
-# Mis tüüpi on järgnevad muutujad:
-# a) vanus = 18
-# b) eesnimi = "Jaak"
-# c) pikkus = 1.65
-# d) kas_on_koolis = True
-# Mis võimalus veel peale True oleks viimast muutujat väärtustada? Kuidas võiks nende
-# Kirjuta kood tüüpide kontrollimiseks.
-#vanus = 18          #int
-#eesnimi = "Jaak"    #str
-#pikkus = 1.65      #float
-#kas_kaib_koolis = True  #bool
-#print(f"vanus {vanus} on: {type(vanus)}")
-#print(f"eesnimi {eesnimi} on: {type(eesnimi)}")
-#print(f"pikkus {pikkus} on: {type(pikkus)}")
-#print(f"kas_kaib_koolis {kas_kaib_koolis} on: {type(kas_kaib_koolis)}")
-
-#4.
-#import math 
-#
-#umbermoodu = float(input("Sisseta puu umbermoot (cm): "))
-#diameeter = umbermoodu / math.pi
-#print("Puu diameeter on:", round(diameeter, 2), "cm")
-
-#5.
-#print('Tere, maailm!')
-#nimi = input('Mis on sinu nimi? ').capitalize()
-
-#print(f'Tere, maailm! Tervitan sind {nimi}! Kui vana sa oled?')
-#print(f'Tere, maailm! Tervitan sind {nimi}! Kui vana sa oled?')
-#vanus = input()
-#print(f'Oi kui tore, sa oled {vanus} aastat vana')
-
-
-
-
-
-#vanus = 18  #int
-#nimi = 'Jaak'  #str
-#pikkus = 16.5  #float
-#kas_käib_koolis = False  #bool
-
-#if kas_käib_koolis == True:
-#    print('Näeme koolis')
-#else:
-#    print('Näeme tööl')
-
-#print(type(vanus))
-#print(type(nimi))
-#print(type(pikkus))
-#print(type(kas_käib_koolis))
-
-
-#from random import randint
-#while True:
-#    kommid = randint(5, 10)
-#    print(f'Laual on {kommid} kommi')
-#    poiss = int(input('Mitu kommi sa soovid võtta? '))
-#    if poiss > kommid:
-#        print('Laual pole nii palju komme')
-#    else:
-#        print(f'Laual on {kommid - poiss} kommi')
-
-
-
-#from math import pi
-
-#ümbermõõdu = float(input('Sisestage ümbermõõdu: '))
-#diameeter = ümbermõõdu / pi
-#print(f'diameetri pikkus on {diameeter}')
-
-
-#a = float(input('Sisestage N: '))
-#b = float(input('Sisestage M: '))
-
-#c = float(a**2+b**2)
-#print(f'käsureal on {c:.2f}')
-
-
-#aeg = float(input("Mitu tundi kulus sõiduks? "))
-#teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
-#kiirus = teepikkus / aeg
-
-#print("Sinu kiirus oli " + str(kiirus) + " km/h")
 # print('Tere, maailm!')
 # nimi = input('Mis on sinu nimi? ').capitalize()
 #
@@ -117,7 +10,7 @@
 #
 #
 #
-#
+#ülesanne 2
 #
 # vanus = 18  #int
 # nimi = 'Jaak'  #str
@@ -134,6 +27,7 @@
 # print(type(pikkus))
 # print(type(kas_käib_koolis))
 #
+#ülesanne 3
 #
 # from random import randint
 # while True:
@@ -145,14 +39,15 @@
 #    else:
 #        print(f'Laual on {kommid - poiss} kommi')
 #
-#
+#ülesanne 4
 #
 # from math import pi
 #
 # ümbermõõdu = float(input('Sisestage ümbermõõdu: '))
 # diameeter = ümbermõõdu / pi
-# print(f'diameetri pikkus on {diameeter}')
+# print(f'diameetri pikkus on {diameeter:.2f}')
 #
+#ülesanne 5
 #
 # a = float(input('Sisestage N: '))
 # b = float(input('Sisestage M: '))
@@ -160,12 +55,15 @@
 # c = float(a**2+b**2)
 # print(f'käsureal on {c:.2f}')
 #
+#ülesanne 6
 #
 # aeg = float(input("Mitu tundi kulus sõiduks? "))
 # teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
 # kiirus = teepikkus / aeg
 #
 # print("Sinu kiirus oli " + str(kiirus) + " km/h")
+
+#ülesanne 7
 
 # numbrid = []
 # for u in range(5):
@@ -178,6 +76,8 @@
 #     result = kesk/len(numbrid)
 # print(result)
 
+#ülesanne 8
+
 # print('  @..@\n'
 #       ' (----)\n'
 #       '( \__/ )\n'
@@ -185,6 +85,8 @@
 #
 #
 #       )
+
+#ülesanne 9
 
 # a = float(input('Sisestage a: '))
 # b = float(input('Sisestage b: '))
@@ -194,8 +96,8 @@
 # print(f'valem: P = a + b + c\n'
 #       f'{ümbermõõdu}')
 
-# 
-# numbrid = []
+# ülesanne 10
+#
 # pitsa_hind = 12.90 * 0.1
 # sõbrad = int(input('Mitu sõbrad läksid Pitsa eest? '))
 # pitsa_hind = pitsa_hind/sõbrad
---
+++ Tund2_vitaliiabdulov.py
@@ -0,0 +1,29 @@
+﻿from math import *
+import math #iport oli valasti tehtud
+print("Ruudu karakteristikud")
+a=float(input('Sisesta ruudu külje pikkus => ')) #float ei olnud
+S=a**2
+print("Ruudu pindala", S)
+P=4*a
+print("Ruudu ümbermõõt", P)
+di=a*math.sqrt(2) #sqrt ei olnud 
+print("Ruudu diagonaal", round(di,2))
+print()
+print("Ristküliku karakteristikud")
+b=float(input("Sisesta ristküliku 1. külje pikkus => ")) #float ei olnud
+c=float(input("Sisesta ristküliku 2. külje pikkus => ")) #float ei olnud
+S=b*c
+print('Ristküliku pindala', round(S, 2))
+P=2*(b+c)
+print("Ristküliku ümbermõõt", round(P, 2))
+di=math.sqrt(b**2+c**2)
+print("Ristküliku diagonaal", round(di, 2))
+print()
+print("Ringi karakteristikud")
+r=float(input("Sisesta ringi raadiusi pikkus => ")) #float ei olnud ja " ei olnud 
+d=2*r
+print("Ringi läbimõõt", round(d, 2)) #, ei olnud
+S=pi*r**2
+print("Ringi pindala", round(S, 2))
+C=2*pi*r #* ei olnud
+print("Ringjoone pikkus", round(C, 2)) #teine ) ei olnud
