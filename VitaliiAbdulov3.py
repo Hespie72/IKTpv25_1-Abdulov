04060af619cc192bc45f63161b03f2260f110cb1
opilane <opilane@HP-CZC2349HT3>
2025-10-09 11:43:07 +0300

    teine haridus

---
+++ VitaliiAbdulov3.py
@@ -0,0 +1,14 @@
+päev=input("Sisesta päev nimetus (näiteks esmaspäev): ")
+#1.
+if päev.lower()=="neljapäev":
+    print("huura täna on programmeerimine")
+#2.
+if päev.lower()=="neljapäev":
+    print("huura täna on programmeerimine")
+else:
+    print("Igatsen, programmeerida tahaks!")
+#3.
+if päev.lower()=="Laupäev" or päev.lower()=="pühapäev":
+    print("Lõpuks, pean tööl käima")
+else:
+    print("Tõõpäev, pean tööl käima!")
