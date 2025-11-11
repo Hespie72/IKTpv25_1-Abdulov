a911ca3f5f0ae1a32af251e87cf45dabc9a4b6b8
hespi <hespi@DESKTOP-5PP6ONH>
2025-11-11 22:19:57 +0200

    update

--- VitaliiAbdulov 3 kontrol töö.py
+++ VitaliiAbdulov 3 kontrol töö.py
@@ -14,28 +14,15 @@
 # else: 
 #     print("siseta õige number")
 #2. Korruta kõik paaritud väärtused vahemikus 0 kuni kasutaja sisestatud arvuni (R).
-R = int(input("R on: "))
-tulemus = 1
+# Пользователь вводит число R
+# R = int(input("Введите число R: "))
 
-for i in range(0, 2 * R ):
-    tulemus = tulemus * i
-    print(tulemus, ",", tulemus * R)
+# n = int(input("Введите верхний предел диапазона: "))
 
-print(f"Tulemus on: {R} on {tulemus}")
-# # 3.
-# import random
-# randomnumber = random.randint(1, 100)
-# katseid = 0
-
-# user = int(input("Arva number 1 kuni 100: "))
-# katseid += 1
-# while user != randomnumber:
-#     if user < randomnumber:
-#         print("Sinu number on liiga väike")
-#     else:
-#         print("Sinu number on liiga suur")
-#     user = int(input("Arva number 1 kuni 100: "))
-#     katseid += 1
-    
-
-# #Loenda paaris- ja paaritud numbrid sisestatud naturaalarvus. Näide: arv 34560 → 3 paarisarvu (4, 6 ja 0) ja 2 paaritut (3 ja 5).
+# for i in range(0, n + 1):
+#     if i % 2 == 0:
+#         print(f"{i} * {R} = {i * R}")
+# 3.
+import random
+randomnumber = random.randint(1, 100)
+n = randint
