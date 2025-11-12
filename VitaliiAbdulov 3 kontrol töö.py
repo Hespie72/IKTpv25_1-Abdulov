9d0255e5c2c1691377190167c30c7e83f61fa783
hespi <hespi@DESKTOP-5PP6ONH>
2025-11-12 20:39:23 +0200

    update 3

--- VitaliiAbdulov 3 kontrol töö.py
+++ VitaliiAbdulov 3 kontrol töö.py
@@ -23,6 +23,37 @@
 #     if i % 2 == 0:
 #         print(f"{i} * {R} = {i * R}")
 # 3.
-import random
-randomnumber = random.randint(1, 100)
-n = randint
+#import random
+
+# N = random.randint(5, 15)
+# print(f"Arvude kogus: {N}")
+
+# arvud = [random.randint(-10, 10) for _ in range(N)]
+# print("Arvud:", arvud)
+
+# positiivsed = sum(1 for arv in arvud if arv > 0)
+
+# print(f"Positiivseid arve on: {positiivsed}")
+# 4.
+# arv = input("Sisesta naturaalarv: ")
+
+# paaris = 0 
+# paaritu = 0  
+
+# for number in arv:
+#     if int(number) % 2 == 0:
+#         paaris += 1
+#     else:
+#         paaritu += 1
+
+# print(f"Arvus {arv} on {paaris} paarisarvu ja {paaritu} paaritut arvu.")
+# 5.
+# A = int(input("Sisesta arv A: "))
+# B = int(input("Sisesta arv B: "))
+
+# if A > B:
+#     A, B = B, A
+
+# summa = sum(range(A, B + 1))
+
+# print(f"Arvude {A} kuni {B} summa on {summa}.")
