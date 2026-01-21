bde3ac8340e137dd98857ff5e2d0841604fc2f8c
hespi <hespi@DESKTOP-5PP6ONH>
2026-01-21 22:29:27 +0200

    updt 2

--- mainaanket.py
+++ mainaanket.py
@@ -1,3 +1,17 @@
 from moduleaanket import *
-print(lisa_uus_kus())
-
+while True:
+    aanket = input("Kas soovite t?ita ankeeti? (jah/ei): ").lower()
+    if aanket == "jah":
+        kusimused()
+        saada_email()
+        break
+    elif aanket == "ei":
+        print("Ankeedi t?itmine katkestatud.")
+        lisakus = input("Kas soovite lisada uue k?simuse? (jah/ei): ").lower()
+        if lisakus == "jah":
+            lisa_uus_kus()
+        else:
+            print("Uue k?simuse lisamine katkestatud.")
+        break
+    else:
+        print("Palun sisesta 'jah' v?i 'ei'.")
